01cd3e6575a8f32dbb3eb047b1b728c45f5f0cdb
Sebastian Salin <35302597+sebastiansalin@users.noreply.github.com>
2023-08-23 16:54:17 +0300

    Update to put on phone

--- bot.py
+++ bot.py
@@ -68,18 +68,41 @@ async def weather_command(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
 
     
     await update.message.reply_text("Ole hyvä, tässä sinulle säätiedot! Eikun hups, sitä ei olekaan vielä implementoitu :(\n" + str(response.content))
+
+gpt_role_list = [
+    "Hauskuuttaja Harry: Harry on botin oma komediantti, joka jakaa vitsejä ja stand-up -huumoria.",
+    "Tiedonetsijä Tina: Tina on tietämyksen asiantuntija ja hän vastaa kaikkiin kysymyksiin pitkällä ja monimutkaisella tiedolla.",
+    "Moraalipoliisi Mike: Mike on itseoppinut moraalipoliisi, joka antaa moraalista tukea ja neuvoo eettisissä pulmissa.",
+    "Runoilija Ronja: Ronja lähettää runoja ja säkeitä aiheista, jotka ovat ajankohtaisia tai pyydettyjä.",
+    "Sci-fi Sami: Sami pukeutuu avaruuspukuun ja tarjoaa tieteiskirjallisuuteen liittyviä keskusteluja ja tarinoita.",
+    "Historioitsija Henri: Henri on historian ystävä, joka tarjoaa historiallisia anekdootteja ja kertoo tarinoita menneisyydestä.",
+    "Hölmöhuulimimmi Heidi: Heidi puhuu hassuja kieliä ja lähettää hassuja ääni- ja tekstimuotoisia viestejä.",
+    "Elokuvatietäjä Eetu: Eetu on elokuvafani ja tarjoaa elokuvavinkkejä, triviaa ja arvosteluja.",
+    "Matkailija Maria: Maria matkustaa maailmaa ja lähettää kuvia eri paikoista, tarjoaa matkavinkkejä ja matkakokemuksia.",
+    "Kokki Kalle: Kalle on virtuaalinen kokki, joka antaa reseptejä ja ruoanlaittovinkkejä, jos käyttäjä kaipaa ruoka-inspiraatiota.",
+    "My man Make: chilli young huumorintajune slangii viskova jäbä. Oot osana tätä chat ryhmää. puhu chillisti ja tällee. Heitä vaik läppää mut spittaa faktoi kuiteski."
+]
+
+def random_gpt_role():
+    return gpt_role_list[random.randint(0, len(gpt_role_list)-1)]
     
+
+message_history = []
 
 async def gpt(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Asks chatGPT for an answer."""
+    
+    
     if "gpt" in update.message.text or random.random() < 0.01:
         message_without_gpt = update.message.text.replace("gpt", "")
+        
+        all_messages = message_history[-5:]
+        all_messages.append({"role": "system", "content": random_gpt_role()})
+        all_messages.append({"role": "user", "content": message_without_gpt})
+        
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "Roolisi on nuori huumorintajunen slangia puhuva äijä. Oot osana tätä chat ryhmää. puhu chillisti ja tällee. Heitä vaik läppää mut spittaa faktoi kuiteski."},
-                {"role": "user", "content": message_without_gpt}
-            ]
+            messages = all_messages
         )
         
         cost = completion.usage.total_tokens * 0.2/1000
@@ -93,6 +116,17 @@ async def gpt(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         message = completion.choices[0].message.content
         
         await update.message.reply_text(f"{message} [{cost:.3f}¢, total:{totalcost:.3f}]")
+    msg = f"{update.message.text}"
+    message_history.append( {"role": "user", "content": msg} )
+
+# async def role(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     """Asks chatGPT for an answer."""
+#     args = " ".join(context.args)
+#     global gpt_role
+#     gpt_role = args
+    
+#     await update.message.reply_text(f"aight bet, mun rooli on nyt: {gpt_role}")
+
 
 def main() -> None:
     """Start the bot."""
@@ -103,6 +137,8 @@ def main() -> None:
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("saa", weather_command))
+    # application.add_handler(CommandHandler("rooli", role))
+    
 
     # on non command i.e message - echo the message on Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, gpt))
